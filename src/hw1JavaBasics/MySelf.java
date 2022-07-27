ddc130edab33b0157d737dbcc8a5520dd2ab4c68
hsbhmd <hsbhmemd@gmail.com>
2022-07-26 20:18:23 -0400

    HW 1

---
+++ src/hw1JavaBasics/MySelf.java
@@ -0,0 +1,15 @@
+package hw1JavaBasics;
+
+//This is single line comment
+/*
+ * The Java comments are the statements that are not executed by the compiler and interpreter. 
+The comments can be used to provide information or explanation about the variable, method, class or any statement.
+ */
+public class MySelf {
+	public static void main(String[] args) {
+		System.out.println("Hasib Ahmed");
+		System.out.println(" 2534 71st street, East Elmhrust Queens, NY11370");
+
+	}
+
+}
