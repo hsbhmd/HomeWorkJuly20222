c3989c7c847bf6d54cc4ed2bc40bf169f2a57ffc
hsbhmd <hsbhmemd@gmail.com>
2022-07-26 20:45:12 -0400

    hw2

---
+++ src/hw2JavaBasics/MyCar.java
@@ -0,0 +1,17 @@
+package hw2JavaBasics;
+
+public class MyCar {
+
+	public static void main(String[] args) {
+		System.out.println("\tMy name is \"Hasib\" . " + "i love to drive. " + "My car info is below:");
+		System.out.print("\nName: Audi");
+		System.out.print("\nColor: Blue");
+		System.out.print("\nModel: S5");
+		System.out.print("\nYear: 2018");
+		System.out.print("\nWheel Driive: all wheel");
+		
+		
+
+	}
+
+}
